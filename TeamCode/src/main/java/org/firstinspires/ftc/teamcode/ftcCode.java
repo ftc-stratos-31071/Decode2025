1704cfe3310de764ece8aff8c01042669bc95c44
neilbaligar-coder <neilbaligar@gmail.com>
2025-11-22 15:46:20 -0500

    new april tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftcCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftcCode.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.hardware.limelightvision.LLResultTypes.FiducialResult;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import java.util.List;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -46,6 +47,27 @@ public class ftcCode extends OpMode {
             telemetry.addData("Ty",llResult.getTy());
             telemetry.addData("Ta",llResult.getTa());
             telemetry.addData("Yaw",botpose.getOrientation().getYaw());
+            int tagId = -1;
+            List<FiducialResult> fiducials = llResult.getFiducialResults();
+            if (fiducials != null && !fiducials.isEmpty()) {
+                FiducialResult firstTag = fiducials.get(0);
+                tagId = firstTag.getFiducialId();
+                telemetry.addData("Tag ID", tagId);
+            }
+            String tag21 = "green purple purple";
+            String tag22 = "purple green purple";
+            String tag23 = "purple purple green";
+            if (tagId == 21){
+                telemetry.addData("Goal",tag21);
+            }
+            if (tagId == 22){
+                telemetry.addData("Goal",tag22);
+            }
+            if (tagId == 23){
+                telemetry.addData("Goal",tag23);
+            }
+
+            telemetry.update();
         }
 
 
